25e172dbeef4ecf6dccd046be9d7ce4387243fa9
dinhnguyenminhhoang <dinhnguyenminhhoang28@gmail.com>
2024-01-13 14:09:44 +0700

    Custom Dynamic Middleware for ApiKey and Permissions #6 section

--- Nodejs/src/routers/index.js
+++ Nodejs/src/routers/index.js
@@ -1,11 +1,12 @@
 "use strict";
 const express = require("express");
+const { apiKey, permission } = require("../auth/checkAuth");
 const router = express.Router();
-router.use(`/v1/api`, require("./access"));
-// router.get("/", (req, res, next) => {
-//     return res.status(200).json({
-//         message: "wellcome nodeJs",
-//     });
-// });
+// check api key
+router.use(apiKey);
+// check permissions
+router.use(permission("0000"));
 
+//router
+router.use(`/v1/api`, require("./access"));
 module.exports = router;
