f63c6cf2f4037bc6b73c3a3cb54989d115c7bc82
dinhnguyenminhhoang <dinhnguyenminhhoang28@gmail.com>
2024-01-31 22:29:43 +0700

    Visited Fields

--- formik/src/components/FormData.js
+++ formik/src/components/FormData.js
@@ -1,20 +1,22 @@
 import { useFormik } from "formik";
 import React from "react";
+
 const initialValues = {
     name: "",
     email: "",
     channel: "",
 };
+
 function FormData() {
     const validate = (values) => {
         let errors = {};
-        if (!values.name?.trim()) {
+        if (!values.name) {
             errors.name = "Required";
         }
-        if (values.email?.trim()) {
+        if (!values.email) {
             errors.email = "Required";
         }
-        if (values.channel?.trim()) {
+        if (!values.channel) {
             errors.channel = "Required";
         }
         return errors;
@@ -23,50 +25,72 @@ function FormData() {
     const handleSubmit = (values) => {
         console.log(values);
     };
+
     const formik = useFormik({
         initialValues,
         onSubmit: handleSubmit,
         validate,
     });
-
+    console.log(formik.touched);
     return (
-        <div className="flex h-screen justify-center items-center ">
+        <div className="flex h-screen justify-center items-center">
             <form
-                onSubmit={formik.onSubmit}
+                onSubmit={formik.handleSubmit}
                 className="flex flex-col gap-4 p-6 border-2 border-gray-900"
             >
-                <label htmlFor="name">Name</label>
-                <input
-                    className="border border-gray-700 rounded-md px-4 py-2"
-                    type="text"
-                    id="name"
-                    name="name"
-                    onChange={formik.handleChange}
-                    value={formik.values.name}
-                />
+                <div>
+                    <label htmlFor="name">Name</label>
+                    <input
+                        className="border border-gray-700 rounded-md px-4 py-2"
+                        type="text"
+                        id="name"
+                        name="name"
+                        onChange={formik.handleChange}
+                        value={formik.values.name}
+                    />
+                    {formik.errors.name && (
+                        <div className="text-red-500">{formik.errors.name}</div>
+                    )}
+                </div>
 
-                <label htmlFor="email">Email</label>
-                <input
-                    className="border border-gray-700 rounded-md px-4 py-2"
-                    type="email"
-                    id="email"
-                    name="email"
-                    onChange={formik.handleChange}
-                    value={formik.values.email}
-                />
+                <div>
+                    <label htmlFor="email">Email</label>
+                    <input
+                        className="border border-gray-700 rounded-md px-4 py-2"
+                        type="email"
+                        id="email"
+                        name="email"
+                        onChange={formik.handleChange}
+                        value={formik.values.email}
+                    />
+                    {formik.errors.email && (
+                        <div className="text-red-500">
+                            {formik.errors.email}
+                        </div>
+                    )}
+                </div>
 
-                <label htmlFor="channel">channel</label>
-                <input
-                    className="border border-gray-700 rounded-md px-4 py-2"
-                    type="text"
-                    id="channel"
-                    name="channel"
-                    onChange={formik.handleChange}
-                    value={formik.values.channel}
-                />
+                <div>
+                    <label htmlFor="channel">Channel</label>
+                    <input
+                        className="border border-gray-700 rounded-md px-4 py-2"
+                        type="text"
+                        id="channel"
+                        name="channel"
+                        onChange={formik.handleChange}
+                        value={formik.values.channel}
+                    />
+                    {formik.errors.channel && (
+                        <div className="text-red-500">
+                            {formik.errors.channel}
+                        </div>
+                    )}
+                </div>
+
                 <button
                     type="submit"
                     className="border-2 px-2 py-4 hover:bg-slate-300"
+                    disabled={Object.keys(formik.errors).length > 0}
                 >
                     Submit
                 </button>
